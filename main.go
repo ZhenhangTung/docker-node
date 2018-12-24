360455482d8b2f0b6f8c79283d3a6b168cd06553
zhenhang.tung <zhenhangtung@gmail.com>
2018-12-24 21:03:00 +0800

    chore(*): update

--- main.go
+++ main.go
@@ -3,16 +3,20 @@ package main
 import (
 	"fmt"
 	"os/exec"
+	"bytes"
 )
 
 func main()  {
 	args := []string{
-		"poster.js",
+		"index.js",
 	}
 	cmd := exec.Command("node", args...)
+	var stdout, stderr bytes.Buffer
+	cmd.Stdout = &stdout
+	cmd.Stderr = &stderr
 	err := cmd.Run()
 	if err != nil {
-		fmt.Printf("failed. err: %v", err)
+		fmt.Printf("failed. err: %v", stderr.String())
 		return
 	}
 	fmt.Println("ok")
